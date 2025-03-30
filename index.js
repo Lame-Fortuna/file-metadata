257ac3d921e5a4618151ddc617da9924e547e755
Pi Patel <pi.patel@gmail.com>
2025-03-31 03:08:12 +0530

    1st

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+const multer = require('multer')
+const path = require('path')
+const fs = require('fs')
 
 var app = express();
 
@@ -11,8 +14,31 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+// Ensuring the directory '/files' exist
+const dir = path.join(__dirname, 'files');
+if (!fs.existsSync(dir)){
+    fs.mkdirSync(dir);
+}
 
+//Mult storage directory
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'files/'); // Store files in the folder
+    },
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + '-' + file.originalname); // Unique filename
+    }
+});
 
+const upload = multer({ storage: storage });
+
+app.post('/api/fileanalyse', upload.array('upfile'), async (req, res)=>{
+  let info = []
+  req.files.forEach(file =>{
+    info.push({"name":file.originalname, "type":file.mimetype, "size":file.size})
+  })
+  res.status(200).json(info)
+})
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
